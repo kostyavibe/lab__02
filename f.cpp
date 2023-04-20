d8f2137046bc376373407bc2f406781efc6699a2
kostyavibe <zdanovkosta55@gmail.com>
2023-04-20 16:44:53 +0300

    commit

--- f.cpp
+++ f.cpp
@@ -1,7 +1,7 @@
 #include
 void main(){
-std::string name = " "
+std::string name = " "// Создаём строковую переменную
 std::cout << "Hello,please input your name" << endl;
-std::cin >> name
+std::cin >> name//Записать своё имя
 std::cout << "Hello world from" << name << endl;
 }
