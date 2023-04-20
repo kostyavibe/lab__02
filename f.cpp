4427157d546f2424e69a1da619668d036e38ab72
kostyavibe <zdanovkosta55@gmail.com>
2023-04-20 15:54:20 +0300

    first

--- f.cpp
+++ f.cpp
@@ -1,5 +1,8 @@
 #include
 using namespace std;
 void main(){
-cout << "Hello world!" << endl;
+string name = " "
+cout << "Hello,please input your name" << endl;
+cin >> name
+cout << "Hello world from" << name << endl;
 }
